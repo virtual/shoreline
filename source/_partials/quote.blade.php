a69b090b6a4859c90ae1bebecf2fd8132e6ffbb6
virtual <jeanine.mt@gmail.com>
2018-06-01 16:17:16 -0600

    quote spacing

--- source/_partials/quote.blade.php
+++ source/_partials/quote.blade.php
@@ -1,11 +1,16 @@
-<figure class="quote-feature">
-  <img src="{{ $img }}" alt="{{ $alt }}" />
-  <blockquote>
-    {{ $desc }}
-  </blockquote>
-  <figcaption>
-      @if($attribution) <strong>{{ $attribution }}</strong> @endif
-      @if(($attribution) && ($attribution2)) <br/> @endif
-    {{ $attribution2 }}
-</figcaption>
+<figure class="quote-feature ">
+  <div class="quote-feature-img">
+    <img src="{{ $img }}" alt="{{ $alt }}" />
+  </div>
+  <div class="quote-feature-text">
+    <blockquote>
+      {{ $desc }}
+    </blockquote>
+    <figcaption>
+        @if($attribution) <strong>{{ $attribution }}</strong> @endif
+        @if(($attribution) && ($attribution2)) <br/> @endif
+      {{ $attribution2 }}
+    </figcaption>
+  </div>
 </figure> 
+
