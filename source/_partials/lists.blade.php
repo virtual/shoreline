681d2d121acd938d2439f2e1eab9cfd80881d273
virtual <jeanine.mt@gmail.com>
2018-07-03 19:58:39 +0000

    add features checklist svg class

--- source/_partials/lists.blade.php
+++ source/_partials/lists.blade.php
@@ -1,8 +1,8 @@
 <div class="row entry">
   <div class="col-sm-4">
     <h4>Features Checklist</h4>
-    <ul>
-      <li>Outcomes &amp; Careers</li>
+    <ul class="features">
+      <li>Outcomes &amp; Careers Outcomes &amp; Careers Outcomes &amp; Careers</li>
       <li>Outcomes &amp; Careers</li>
       <li>Outcomes &amp; Careers</li>
       <li>Outcomes &amp; Careers</li>
@@ -12,83 +12,22 @@
   <div class="col-sm-4">
     <h4>Ordered List</h4>
     <ol>
-        <li>Outcomes &amp; Careers</li>
-        <li>Outcomes &amp; Careers and Outcomes &amp; Careers</li>
-        <li>Outcomes &amp; Careers</li>
-        <li>Outcomes &amp; Careers<ol>
-          
-          <li>Hmm.... &amp; Careers<ol>
-          
-            <li>Hmm.... &amp; Careers</li>
-            <li>Outcomes &amp; Careers</li>
-            <li>Outcomes &amp; Careers a maintainable technologi<ol>
-          
-              <li>Hmm.... &amp; Careers</li>
-              <li>Outcomes &amp; Careers</li>
-              <li>Outcomes &amp; Careers a maintainable technologi</li>
-            </ol></li>
-          </ol></li>
-          <li>Outcomes &amp; Careers</li>
-          <li>Outcomes &amp; Careers a maintainable technologi<ul>
-          
-            <li>Hmm.... &amp; Careers</li>
-            <li>Outcomes &amp; Careers</li>
-            <li>Outcomes &amp; Careers oss-platform paradigms after B2C infomediaries. D</li>
-          </ul></li>
-        </ol></li>
-        <li>Outcomes &amp; Careers</li>
-        <li>Outcomes &amp; Careers</li>
-        <li>Outcomes &amp; Careers and more Outcomes &amp; Careers oss-platform paradigms after B2C infomediaries. D</li>
-        <li>Outcomes &amp; Careers<ul>
-          
-          <li>Hmm.... &amp; Careers</li>
-          <li>Outcomes &amp; Careers</li>
-          <li>Outcomes &amp; Careers oss-platform paradigms after B2C infomediaries. D</li>
-        </ul></li>
-        <li>Outcomes &amp; Careers and Outcomes &amp; Careers oss-platform paradigms after B2C infomediaries. D</li>
-        <li>Outcomes &amp; Careers</li>
-        <li>Outcomes &amp; Careers</li>
-        <li>Outcomes &amp; Careers</li>
-        <li>Outcomes &amp; Careers</li>
-        <li>Outcomes &amp; Careers and Outcomes &amp; Careers oss-platform paradigms after B2C infomediaries. D</li>
-        <li>Outcomes &amp; Careers</li>
-        <li>Outcomes &amp; Careers</li>
-        <li>Outcomes &amp; Careers</li>
+      <li>Outcomes &amp; Careers</li>
+      <li>Outcomes &amp; Careers and Outcomes &amp; Careers</li>
+      <li>Outcomes &amp; Careers</li>
+      <li>Outcomes &amp; Careers</li>
+      <li>Outcomes &amp; Careers</li>
+      <li>Outcomes &amp; Careers</li>
     </ol>
   </div>
   <div class="col-sm-4">
     <h4>Unordered List</h4>
     <ul>
-      <li>Outcomes &amp; Careers<ul>
-          
-        <li>Hmm.... &amp; Careers</li>
+      <li>Outcomes &amp; Careers
         <li>Outcomes &amp; Careers</li>
-        <li>Outcomes &amp; Careers oss-platform paradigms after B2C infomediaries. D<ul>
-          
-          <li>Hmm.... &amp; Careers</li>
-          <li>Outcomes &amp; Careers</li>
-          <li>Outcomes &amp; Careers oss-platform paradigms after B2C infomediaries. D</li>
-        </ul></li>
-      </ul></li>
-      <li>Outcomes &amp; Careers<ol>
-          
-        <li>Hmm.... &amp; Careers</li>
         <li>Outcomes &amp; Careers</li>
-        <li>Outcomes &amp; Careers a maintainable technologi<ol>
-          
-          <li>Hmm.... &amp; Careers</li>
-          <li>Outcomes &amp; Careers</li>
-          <li>Outcomes &amp; Careers a maintainable technologi</li>
-        </ol></li>
-      </ol></li>
-      <li>Holisticly embrace cross-platform paradigms after B2C infomediaries. Dramatically build.</li>
-      <li>Uniquely facilitate team building.<ul>
-          
-          <li>Hmm.... &amp; Careers</li>
-          <li>Outcomes &amp; Careers</li>
-          <li>Outcomes &amp; Careers a maintainable technologi</li>
-        </ul></li>
-      <li>Rapidiously visualize cutting-edge functionalities via maintainable technologies.</li>
+        <li>Rapidiously visualize cutting-edge functionalities via maintainable technologies.</li>
+        <li>Outcomes &amp; Careers and Outcomes &amp; Careers</li>
     </ul>
   </div>
 </div>
