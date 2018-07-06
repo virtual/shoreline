c1b2dff8b15d2b11034145379c694a8965a98cf6
virtual <jeanine.mt@gmail.com>
2018-07-06 16:25:50 +0000

    search box

--- source/_partials/searchform.blade.php
+++ source/_partials/searchform.blade.php
@@ -1,9 +1,14 @@
 <form id="siteSearch" class="siteSearch" role="search" method="GET" action="/search/SearchRedir.aspx">
-  <div class="main-search" role="search">
-    <label for="EnterSearch" class="hidden">Enter search terms</label>
-    <input id="EnterSearch" type="text" name="EnterSearch" class="search-main" placeholder="Search" value="">
-    <label for="SubmitSearch" class="hidden">Perform search</label>
-    <input id="SubmitSearch" name="SubmitSearch" type="submit" class="submit" value="Perform search">
+  <div class="input-group main-search" role="search">
+    <span class="input-group-prepend">
+      <button class="input-group-text bg-transparent" id="SubmitSearch" name="SubmitSearch" type="submit">
+        <span class="fa fa-search primary"></span>
+        <span class="sr-only">Search</span>
+      </button>
+    </span>
+    <label for="EnterSearch" class="sr-only">Enter search terms</label>
+    <input id="EnterSearch" type="search" name="EnterSearch" class="form-control py-2 border-left-0 border" placeholder="Search"
+      value="">
     <span class="search-open"></span>
   </div>
 </form>
