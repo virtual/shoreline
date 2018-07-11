07a8851ec5d9427bb5c27b0171fee7427fa1016e
virtual <jeanine.mt@gmail.com>
2018-07-11 21:52:53 +0000

    mobile arrows, fx

--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -4,7 +4,7 @@
   </div>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-lg-3 header-logo d-none d-lg-block">
+      <div class="col-lg-2 col-xl-3 header-logo d-none d-lg-block">
 
         @if (trim($__env->yieldContent('homepage')))
         <!-- H1 on homepage only -->
@@ -22,7 +22,7 @@
         @endif
 
       </div>
-      <div class="col-lg-9">
+      <div class="col-lg-10 col-xl-9">
         <div class="header-actions d-none d-lg-flex">
         <div class="navbar-container">
 
