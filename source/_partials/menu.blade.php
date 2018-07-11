1b2a18ee10105d98f93938fa8150db0883786a65
virtual <jeanine.mt@gmail.com>
2018-07-11 14:31:57 +0000

    mobile menu button

--- source/_partials/menu.blade.php
+++ source/_partials/menu.blade.php
@@ -1,151 +1,141 @@
-<nav class="nav nav-underline megamenu navbar-expand-lg navmenu">
-    <h2 class="sr-only">Navigation</h2>
-    <div class="container-fluid">
-      <div class="d-flex d-block d-lg-none justify-content-between">
-        
-        <button class="navbar-toggler primary" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse"
-          aria-expanded="false" aria-label="Toggle navigation">
-           
-          <?xml version="1.0" encoding="utf-8"?>
-<svg version="1.1" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 80 70" style="enable-background:new 0 0 80 70;" xml:space="preserve">
-<style type="text/css">
-	.st0{fill:#12685E;}
-</style>
-<rect id="XMLID_1_" class="st0" width="80" height="10"/>
-<rect id="XMLID_3_" y="30" class="st0" width="80" height="10"/>
-<rect id="XMLID_6_" y="60" class="st0" width="60" height="10"/>
-</svg>
- Menu
-        </button>
-        <a class="navbar-brand" href="/">
-          Shoreline <small>Community College</small>
-        </a>
-        <button class="navbar-toggler primary" type="button" data-toggle="collapse" data-target="#searchCollapse" aria-controls="searchCollapse"
-          aria-expanded="false" aria-label="Toggle search">
-          <span aria-hidden="true" class="fas fa-search"></span>
-          <span class="sr-only">Search</span>
-        </button>
-      </div>
-      <div class="collapse navbar-collapse" id="searchCollapse">
-        <div class="container">
-          <div class="searchform-mobile">
-            @include ('_partials.searchform')
-          </div>
-
-        </div>
-      </div>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-
-        <ul class="distributed">
-          <li class="dropdown">
-            <a href="/about-shoreline/default.aspx">About Shoreline
-              <span role="link" class="dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="sr-only">Toggle Dropdown</span>
-              </span>
-            </a>
-
-            <div class="dropdown-menu">
-              <ul>
-                  <li><a href="/about-shoreline/facts.aspx">Shoreline at a Glance</a></li>
-                  <li><a href="/about-shoreline/community-standard-statement.aspx">Community Standard</a></li>
-                  <li><a href="/about-shoreline/president.aspx">Shoreline's Leadership</a></li>
-                  <li><a href="/faculty/">Our Faculty</a></li>
-                  <li><a href="/about-shoreline/contact-us.aspx">Contact Us</a></li>
-                  <li><a href="http://app.shoreline.edu/campus-directory/">Employee Directory</a></li>
-                  <li><a href="/map/">Maps &amp; Directions</a></li>
-                  <li><a href="/apply-and-aid/campus-tour.aspx">Schedule a Visit</a></li>
-              </ul>
-            </div>
-          </li>
-
-
-          <li class="dropdown">
-              <a href="/areas-of-study/degrees-certificates.aspx">Areas of Study
-              <span role="link" class="dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="sr-only">Toggle Dropdown</span>
-              </span>
-            </a>
-
-            <div class="dropdown-menu">
-              <ul>
-                  <li><a href="/areas-of-study/what-we-offer.aspx">What We Offer</a></li>
-                  <li><a href="/areas-of-study/programs.aspx">Browse all Programs</a></li>
-                  <li><a href="/areas-of-study/programs-transfer.aspx">University Transfer</a></li>
-                  <li><a href="/honors/">Honors College</a></li>
-                  <li><a href="/areas-of-study/programs-vocational.aspx">Professional &amp; Technical</a></li>
-                  <li><a href="/international/">International Education</a></li>
-                  <li><a href="/transitional-programs/">ESL, HS21, I-BEST, GED</a></li>
-                  <li><a href="/virtual-campus/">Online</a></li>
-                  <li><a href="/high-school-programs/">High School</a></li>
-                  <li><a href="/workforce/plus-50/">Continuing Education</a></li>
-              </ul>
-            </div>
-          </li>
-          <li class="dropdown">
-              <a href="/apply-and-aid/default.aspx">Apply &amp; Aid
-              <span role="link" class="dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="sr-only">Toggle Dropdown</span>
-              </span>
-            </a>
-
-            <div class="dropdown-menu">
-
-
-              <ul>
-                  <li><a href="/apply-and-aid/apply.aspx">How to Apply</a></li>
-                  <li><a href="/apply-and-aid/new-student-checklist.aspx">New Student Checklist</a></li>
-                  <li><a href="/apply-and-aid/financial-aid/">Financial Aid</a></li>
-                  <li><a href="/apply-and-aid/financial-aid/sources-on-campus.aspx#vocational">Funding for Career Training</a></li>
-                  <li><a href="/apply-and-aid/financial-aid/sources-on-campus.aspx">Sources of Aid</a></li>
-                  <li><a href="/apply-and-aid/financial-aid/types.aspx">Types of Aid</a></li>
-                  <li><a href="/calendars/academic.aspx">Academic Calendar</a></li>
-                  <li><a href="/apply-and-aid/cashiering/">Tuition &amp; Fees</a></li>
-
-              </ul>
-
-            </div>
-          </li>
-          <li class="dropdown">
-              <a href="/apply-and-aid/campus-life.aspx">Campus Life
-              <span role="link" class="dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="sr-only">Toggle Dropdown</span>
-              </span>
-            </a>
-
-            <div class="dropdown-menu">
-              <ul>
-                  <li><a href="/currentstudents/default.aspx">Current Students</a></li>
-                  <li><a href="/athletics/">Athletics</a></li>
-                  <li><a href="http://asg.shoreline.edu">Student Government</a></li>
-                  <li><a href="http://asg.shoreline.edu/rso/">Clubs &amp; Organizations</a></li>
-                  <li><a href="http://www.theebbtide.com" target="_blank">Ebbtide - Student Newspaper</a></li>
-                  <li><a href="/athletics/intramurals.aspx">Health &amp; Wellness</a></li>
-                  <li><a href="http://library.shoreline.edu/home">Library &amp; Technology Center</a></li>
-                  <li><a href="/bookstore/">Bookstore</a></li>
-                  <li><a href="/oss/veterans/">Veterans Programs</a></li>
-              </ul>
-            </div>
-          </li>
-          <li class="dropdown">
-              <a href="/about-shoreline/news-and-events.aspx">News &amp; Events
-              <span role="link" class="dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="sr-only">Toggle Dropdown</span>
-              </span>
-            </a>
-
-            <div class="dropdown-menu">
-              <ul>
-                  <li><a href="/calendars/">Calendar of Events</a></li>
-                  <li><a href="/theater/default.aspx">Theater</a></li>
-                  <li><a href="http://news.shoreline.edu">News</a></li>
-              </ul>
-            </div>
-          </li> 
-        </ul>
-        <div class="d-block d-lg-none"> 
-          @include ('_partials.quicklinks') 
+<!-- TODO: review if this should have class .row -->
+<nav class="row nav nav-underline megamenu navbar-expand-lg navmenu">
+  <h2 class="sr-only">Navigation</h2>
+  <div class="container-fluid">
+    <div class="d-flex d-block d-lg-none justify-content-between">
+      
+      <button id="mobile-menu-button" class="navbar-toggler primary" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse"
+        aria-expanded="false" aria-label="Toggle navigation">
+        @include ('_svg.menu') 
+      <span class="menubartext">Menu</span>
+      </button>
+      <a class="navbar-brand" href="/">
+        Shoreline <small>Community College</small>
+      </a>
+      <button class="navbar-toggler primary" type="button" data-toggle="collapse" data-target="#searchCollapse" aria-controls="searchCollapse"
+        aria-expanded="false" aria-label="Toggle search">
+        <span aria-hidden="true" class="fas fa-search"></span>
+        <span class="sr-only">Search</span>
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="searchCollapse">
+      <div class="container">
+        <div class="searchform-mobile">
+          @include ('_partials.searchform')
         </div>
       </div>
     </div>
-  </nav>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+
+      <ul class="distributed">
+        <li class="dropdown">
+          <a href="/about-shoreline/default.aspx">About Shoreline
+            <span role="link" class="dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <span class="sr-only">Toggle Dropdown</span>
+            </span>
+          </a>
+
+          <div class="dropdown-menu">
+            <ul>
+                <li><a href="/about-shoreline/facts.aspx">Shoreline at a Glance</a></li>
+                <li><a href="/about-shoreline/community-standard-statement.aspx">Community Standard</a></li>
+                <li><a href="/about-shoreline/president.aspx">Shoreline's Leadership</a></li>
+                <li><a href="/faculty/">Our Faculty</a></li>
+                <li><a href="/about-shoreline/contact-us.aspx">Contact Us</a></li>
+                <li><a href="http://app.shoreline.edu/campus-directory/">Employee Directory</a></li>
+                <li><a href="/map/">Maps &amp; Directions</a></li>
+                <li><a href="/apply-and-aid/campus-tour.aspx">Schedule a Visit</a></li>
+            </ul>
+          </div>
+        </li>
+
+
+        <li class="dropdown">
+            <a href="/areas-of-study/degrees-certificates.aspx">Areas of Study
+            <span role="link" class="dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <span class="sr-only">Toggle Dropdown</span>
+            </span>
+          </a>
+
+          <div class="dropdown-menu">
+            <ul>
+                <li><a href="/areas-of-study/what-we-offer.aspx">What We Offer</a></li>
+                <li><a href="/areas-of-study/programs.aspx">Browse all Programs</a></li>
+                <li><a href="/areas-of-study/programs-transfer.aspx">University Transfer</a></li>
+                <li><a href="/honors/">Honors College</a></li>
+                <li><a href="/areas-of-study/programs-vocational.aspx">Professional &amp; Technical</a></li>
+                <li><a href="/international/">International Education</a></li>
+                <li><a href="/transitional-programs/">ESL, HS21, I-BEST, GED</a></li>
+                <li><a href="/virtual-campus/">Online</a></li>
+                <li><a href="/high-school-programs/">High School</a></li>
+                <li><a href="/workforce/plus-50/">Continuing Education</a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="dropdown">
+            <a href="/apply-and-aid/default.aspx">Apply &amp; Aid
+            <span role="link" class="dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <span class="sr-only">Toggle Dropdown</span>
+            </span>
+          </a>
+
+          <div class="dropdown-menu">
+
+
+            <ul>
+                <li><a href="/apply-and-aid/apply.aspx">How to Apply</a></li>
+                <li><a href="/apply-and-aid/new-student-checklist.aspx">New Student Checklist</a></li>
+                <li><a href="/apply-and-aid/financial-aid/">Financial Aid</a></li>
+                <li><a href="/apply-and-aid/financial-aid/sources-on-campus.aspx#vocational">Funding for Career Training</a></li>
+                <li><a href="/apply-and-aid/financial-aid/sources-on-campus.aspx">Sources of Aid</a></li>
+                <li><a href="/apply-and-aid/financial-aid/types.aspx">Types of Aid</a></li>
+                <li><a href="/calendars/academic.aspx">Academic Calendar</a></li>
+                <li><a href="/apply-and-aid/cashiering/">Tuition &amp; Fees</a></li>
+
+            </ul>
+
+          </div>
+        </li>
+        <li class="dropdown">
+            <a href="/apply-and-aid/campus-life.aspx">Campus Life
+            <span role="link" class="dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <span class="sr-only">Toggle Dropdown</span>
+            </span>
+          </a>
+
+          <div class="dropdown-menu">
+            <ul>
+                <li><a href="/currentstudents/default.aspx">Current Students</a></li>
+                <li><a href="/athletics/">Athletics</a></li>
+                <li><a href="http://asg.shoreline.edu">Student Government</a></li>
+                <li><a href="http://asg.shoreline.edu/rso/">Clubs &amp; Organizations</a></li>
+                <li><a href="http://www.theebbtide.com" target="_blank">Ebbtide - Student Newspaper</a></li>
+                <li><a href="/athletics/intramurals.aspx">Health &amp; Wellness</a></li>
+                <li><a href="http://library.shoreline.edu/home">Library &amp; Technology Center</a></li>
+                <li><a href="/bookstore/">Bookstore</a></li>
+                <li><a href="/oss/veterans/">Veterans Programs</a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="dropdown">
+            <a href="/about-shoreline/news-and-events.aspx">News &amp; Events
+            <span role="link" class="dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <span class="sr-only">Toggle Dropdown</span>
+            </span>
+          </a>
+
+          <div class="dropdown-menu">
+            <ul>
+                <li><a href="/calendars/">Calendar of Events</a></li>
+                <li><a href="/theater/default.aspx">Theater</a></li>
+                <li><a href="http://news.shoreline.edu">News</a></li>
+            </ul>
+          </div>
+        </li> 
+      </ul>
+      <div class="d-block d-lg-none"> 
+        @include ('_partials.quicklinks') 
+      </div>
+    </div>
+  </div>
+</nav>
---
+++ source/_svg/menu.blade.php
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="utf-8"?>
+<svg version="1.1" class="menubaricon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 80 70" style="enable-background:new 0 0 80 70;" xml:space="preserve">
+<rect class="menubar1" width="80" height="10"/>
+<rect class="menubar2" y="30" width="80" height="10"/>
+<rect class="menubar3" y="60" width="60" height="10"/>
+</svg>
