f55653b0ff37af7e0036e34ad1d844d4996fa1fe
virtual <jeanine.mt@gmail.com>
2018-07-17 15:55:03 +0000

    expand toggle button on overview

--- source/_partials/overview.blade.php
+++ source/_partials/overview.blade.php
@@ -11,6 +11,40 @@
     <dt class="icon-bg-map-marker">Location </dt>
     <dd> On Campus</dd>
     <dt class="icon-bg-credit-card">Tuition &amp; Fees </dt>
-    <dd>  Toggle area</dd>
+    <dd>  
+        <button class="btn-toggle btn" type="button" data-toggle="collapse" data-target="#tuitionfees" aria-expanded="false" aria-controls="tuitionfees">
+          Toggle to expand <span class="sr-only">tuition and fees</span> details </button>
+        <div class="collapse" id="tuitionfees">
+            <table class="data">
+                <thead><tr>
+                  <th>Entry Header 1</th>
+                  <th>Entry Header 2</th>
+                  <th>Entry Header 3</th>
+                  <th>Entry Header 4</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>Entry First Line 1</td>
+                  <td>Entry First Line 2</td>
+                  <td>Entry First Line 3</td>
+                  <td>Entry First Line 4</td>
+                </tr>
+                <tr>
+                  <td>Entry Line 1</td>
+                  <td>Entry Line 2</td>
+                  <td>Entry Line 3</td>
+                  <td>Entry Line 4</td>
+                </tr>
+                <tr>
+                  <td>Entry Last Line 1</td>
+                  <td>Entry Last Line 2</td>
+                  <td>Entry Last Line 3</td>
+                  <td>Entry Last Line 4</td>
+                </tr>
+              </tbody></table>
+
+</div>
+    </dd>
   </dl>
 </section>
--- source/components.blade.php
+++ source/components.blade.php
@@ -14,8 +14,8 @@
 
 
 
-<section class="sectional section1 sectional-padded theme-01 sectional-inverted ">
-    <div class="inner">
+<section class="sectional section1 sectional-padded ">
+    <div class="container-fluid">
         @include('_partials.card', [
           'heading' => 'Get help with enrollment',
           'desc' => 'Our enrollment services team is here to help with financial aid, how to apply, and all the things that make Shoreline a great place to study and pursue any of our more than 100 degrees and certificates!',
@@ -32,11 +32,11 @@
           'theme' => 'secondary',
           'link' => 'http://jigsaw.tighten.co/docs/blade-templates-and-partials/'          
           ])
-    </div>
+ 
  </section>
 
- <section class="sectional theme-02 ">
-    <div class="inner py-5">
+ <section>
+    <div class="container-fluid py-5">
       <div class="slick-slider">
         @include('_partials.quote', [ 
           'desc' => "As corny as it sounds, it has been amazing to be here and have the opportunities that I've had…",
