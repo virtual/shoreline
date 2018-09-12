5b7c74ffcf6c83ce74008c760ac356a814b6a687
virtual <jeanine.mt@gmail.com>
2018-09-12 16:36:58 +0000

    style video comp

--- source/_partials/video.blade.php
+++ source/_partials/video.blade.php
@@ -1,11 +1,11 @@
-<div class="video-box row">
-  <div class="col-sm-5">
+<div class="video-box">
+  <div class="video-box-image">
     <a class="video-lightbox" href="https://www.youtube.com/embed/{{ $youtubeID }}?rel=0&amp;autoplay=1">
       <span class="btn btn-secondary icon-watch"> </span>
       <img src="{{ $page->baseUrl }}{{$img}}" alt="Watch {{$heading}}" />
     </a>
   </div>
-  <div class="col-sm-7">
+  <div class="video-box-content">
     <h3 class="card-title">{{ $heading }}</h3>
     <p class="card-text">{{ $desc }}</p>
   </div>
