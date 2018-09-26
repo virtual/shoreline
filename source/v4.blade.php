4f7f252d5c7509a50be41f23fd7aca8dabeebf46
virtual <jeanine.mt@gmail.com>
2018-09-26 22:51:36 +0000

    fix most IE layout issues; TODO news

---
+++ source/v4.blade.php
@@ -0,0 +1,17 @@
+@extends('_layouts.master')
+
+@section('breadcrumb')
+<ol>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Explore Programs</a></li>
+    <li><a class="active" aria-current="page" href="#">Visual Communications Technology</a></li>
+  </ol>
+@endsection
+   
+
+ 
+@section('content')
+      <h1>Not much here</h1>
+      <p>Empty template, looks nice on newer (not IE 11) browsers</p>
+@endsection
+  
