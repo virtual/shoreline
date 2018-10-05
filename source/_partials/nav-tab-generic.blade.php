0a7fc4c576fcb24c7bd2fcabc5115808293f35fe
virtual <jeanine.mt@gmail.com>
2018-10-05 14:10:34 +0000

    generic tab for snippet glossary

---
+++ source/_partials/nav-tab-generic.blade.php
@@ -0,0 +1,30 @@
+<ul class="list-unstyled nav nav-tabs">
+  <li>
+    <a class="nav-link active" id="tab1" data-toggle="tab" href="#tab1-content" aria-controls="tab1-content" aria-selected="true">Tab1</a>
+  </li>
+  <li>
+    <a class="nav-link" id="tab2" data-toggle="tab" href="#tab2-content" aria-controls="tab2-content" aria-selected="false">Tab2</a>
+  </li>
+  <li>
+    <a class="nav-link" id="tab3" data-toggle="tab" href="#tab3-content" aria-controls="tab3-content" aria-selected="false">Tab3</a>
+  </li>
+
+</ul>
+<div class="tab-content">
+  <div class="tab-pane fade show active" id="tab1-content" aria-labelledby="tab1">
+    <p>Tab 1 -- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum nisi arcu, vitae tempus nisl mattis ut.</p>
+    <p>Tab 1 -- Quisque sollicitudin ex quis sapien imperdiet, a ultricies elit laoreet. Duis nec consequat dui lorem.</p>
+  </div>
+
+  <div class="tab-pane fade" id="tab2-content" aria-labelledby="tab2">
+      <p>Tab 2 -- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum nisi arcu, vitae tempus nisl mattis ut.</p>
+      <p>Tab 2 -- Quisque sollicitudin ex quis sapien imperdiet, a ultricies elit laoreet. Duis nec consequat dui lorem.</p>
+  </div>
+
+
+  <div class="tab-pane fade" id="tab3-content" aria-labelledby="tab3">
+      <p>Tab 3 -- Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum nisi arcu, vitae tempus nisl mattis ut.</p>
+      <p>Tab 3 -- Quisque sollicitudin ex quis sapien imperdiet, a ultricies elit laoreet. Duis nec consequat dui lorem.</p>
+  </div>
+
+</div>
