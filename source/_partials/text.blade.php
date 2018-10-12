55b6674dba53ca7e0e15da47ed41c1597ad857ee
virtual <jeanine.mt@gmail.com>
2018-10-12 10:53:06 +0000

    reimplement pdf external icons for fa 5

--- source/_partials/text.blade.php
+++ source/_partials/text.blade.php
@@ -1,6 +1,6 @@
 
 <p class="xxl">Lorem ipsum dolor sit amet.</p> 
-<p class="xl">Lorem ipsum dolor sit amet.</p>
+<p class="xl">Lorem ipsum dolor <a href="#" class="fa-link-pdf">sit amet</a>.</p>
 <p class="large">Lorem ipsum dolor sit amet.</p>
-<p class="medium">Lorem ipsum dolor sit amet.</p>
+<p class="medium">Lorem <a href="#" class="fa-link-external">sit amet</a> dolor sit amet.</p>
 <p class="small">Lorem ipsum dolor sit amet.</p>
