d4fa10fc729a4414fea47390496d24751881aa6d
virtual <jeanine.mt@gmail.com>
2019-06-10 13:57:38 +0000

    buttons and alert style

--- source/timeline.blade.php
+++ source/timeline.blade.php
@@ -8,28 +8,28 @@
     <div class="line text-muted"></div>
     <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
         @include('_partials.timelinelist', [
-        'heading' => 'Build components & templates (June 5–Sept 13)',
+        'heading' => 'Build components & templates (June 5–Sept 13, 2018)',
         'desc' => "Build components into code; working demo available on Github. Testing is done iteratively.",
         'id' => '1', 
         'img' => ''
         ])
 
         @include('_partials.timelinelist', [
-        'heading' => 'Accessibility review (Aug 20–Sept 3)',
+        'heading' => 'Accessibility review (Aug 20–Sept 3, 2018)',
         'desc' => "Shoreline's Accessibility team reviews and suggest changes for implementation",
         'id' => '2', 
         'img' => ''
         ])
         
         @include('_partials.timelinelist', [
-        'heading' => 'Finalize program options in multiedit (Sept 3)',
+        'heading' => 'Finalize program options in multiedit (Sept 3, 2018)',
         'desc' => "Finalize PCF for OU's migration of Excel to PCF",
         'id' => '2a', 
         'img' => ''
         ])
         
         @include('_partials.timelinelist', [
-        'heading' => 'Acalog goes live  (Sept 1–30)',
+        'heading' => 'Acalog goes live  (Sept 1–30, 2018)',
         'desc' => "
         ",
         'id' => '4', 
@@ -37,7 +37,7 @@
         ])
             
         @include('_partials.timelinelist', [
-        'heading' => 'Code review (Sept 13–27)',
+        'heading' => 'Code review (Sept 13–27, 2018)',
         'desc' => "Shoreline's team goes through demos and approves functionality. All HTML design files review and analyzed and verified to be functioning as expected, plus W3C and Accessibility Compliant. Functional Specification meeting(s) via webex take place to go through all elements in each page type to define the development plan. Report any issues, bugs, questions, or requests back to Jeanine.
         ",
         'id' => '3', 
@@ -45,7 +45,7 @@
         ])
           
         @include('_partials.timelinelist', [
-        'heading' => 'Deadline for Program Option info and department pages (Sept 24)',
+        'heading' => 'Deadline for Program Option info and department pages (Sept 24, 2018)',
         'desc' => "Program option info due and submitted to OU for verification and to prep for migration.
         ",
         'id' => '7', 
@@ -53,7 +53,7 @@
         ])
 
         @include('_partials.timelinelist', [
-        'heading' => 'Backup test site in OU Campus (Sept 28)',
+        'heading' => 'Backup test site in OU Campus (Sept 28, 2018)',
         'desc' => "Jeanine to backup and download test site in order to reference all staging files prior to update on Oct 1-5.
         ",
         'id' => '7c', 
@@ -61,7 +61,7 @@
         ])
 
         @include('_partials.timelinelist', [
-        'heading' => 'Code updates (Sept 28–Oct 5)',
+        'heading' => 'Code updates (Sept 28–Oct 5, 2018)',
         'desc' => "Dev fixes.
         ",
         'id' => '7d', 
@@ -69,7 +69,7 @@
         ])
               
         @include('_partials.timelinelist', [
-        'heading' => 'Duplicate www site to test site in OU Campus (Oct 1–5)',
+        'heading' => 'Duplicate www site to test site in OU Campus (Oct 1–5, 2018)',
         'desc' => "Gavin to remove test site (if possible) and duplicate www to new test site.
         ",
         'id' => '7e', 
@@ -77,7 +77,7 @@
         ])
      
         @include('_partials.timelinelist', [
-        'heading' => 'Implement code on test server (Oct 8-12)',
+        'heading' => 'Implement code on test server (Oct 8-12, 2018)',
         'desc' => "Update page design and template files. Update snippets to function similarly on new framework.
         ",
         'id' => '5', 
@@ -85,7 +85,7 @@
         ])
 
         @include('_partials.timelinelist', [
-        'heading' => 'Review and test existing content into updated templates (Oct 15–19)',
+        'heading' => 'Review and test existing content into updated templates (Oct 15–19, 2018)',
         'desc' => "Shoreline team tests creating new pages using existing snippets; check and report any inconsistencies.
         ",
         'id' => '6', 
@@ -93,7 +93,7 @@
         ])
 
         @include('_partials.timelinelist', [
-        'heading' => 'Code updates (Oct 22–26)',
+        'heading' => 'Code updates (Oct 22–26, 2018)',
         'desc' => "Dev fixes.
         ",
         'id' => '7a', 
@@ -101,7 +101,7 @@
         ])
 
         @include('_partials.timelinelist', [
-        'heading' => 'Implementation process for new theme on www; republish website (early Nov 5)',
+        'heading' => 'Implementation process for new theme on www; republish website (early Nov 5, 2018)',
         'desc' => "Verify working as expected.
         ",
         'id' => '8', 
@@ -109,10 +109,18 @@
         ])
 
         @include('_partials.timelinelist', [
-        'heading' => 'OU Migration for Program options and department pages (Nov 6-9)',
+        'heading' => 'OU Migration for Program options and department pages (Nov 6-9, 2018)',
         'desc' => "OU creates program options. Need to discuss when the file structure is built out--as well as program parent information.
         ",
         'id' => '9', 
+        'img' => ''
+        ])
+
+        
+        @include('_partials.timelinelist', [
+        'heading' => 'Wordpress Theme (June 30, 2019)',
+        'desc' => "Develop Wordpress theme for news.shoreline.edu        ",
+        'id' => '11', 
         'img' => ''
         ])
     </div>
