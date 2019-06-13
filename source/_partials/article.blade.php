a8f0fcfe845e1273fc58476c6999d169952d4d4d
virtual <jeanine.mt@gmail.com>
2019-06-13 14:57:47 +0000

    add stacked article version

--- source/_partials/article.blade.php
+++ source/_partials/article.blade.php
@@ -1,4 +1,4 @@
-<article class="news-article"> 
+@if(!empty($class))<article class="news-article {{ $class }}">@else <article class="news-article">  @endif
   @if(!empty($img)) <div class="image-container"><a href="{{ $link }}"><img src="{{ $page->baseUrl }}{{$img}}" alt="{{$alt}}"></a></div>  @endif
   <div class="article-info-container">
       @if(!empty($datetime))<p class="datetime"><time datetime="{{$datetime}}"><?php echo date("F j, Y", strtotime($datetime)) ?> </time></p>@endif
--- source/news.blade.php
+++ source/news.blade.php
@@ -49,7 +49,16 @@
 
 @section('content')
 <h1>News</h1>
-
+ 
+    @include('_partials.article', [
+    'class' => 'news-article-stacked',
+    'heading' => 'Seven back-to-school tips for college students',
+    'desc' => 'Duis nec consequat dui. Praesent gravida diam at ex pharetra porta. Vivamus at nisi massa. Nam porta…',
+    'alt' => 'students at a table',
+    'img' => '/resources/img/news/studying-in-library_news-crop.jpg',           
+    'link' => 'https://news.shoreline.edu/2018/09/13/seven-back-to-school-tips-for-college-students/',
+    'datetime' => '2018-09-13 11:31'
+    ])  
 <div class="news-no-description">
 <div class="row">
   <div class="col-sm-4">@include('_partials.article', [
