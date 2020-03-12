cbf25acc7ab78646f02f9f3b0f689333ec0f8eee
virtual <jeanine.mt@gmail.com>
2020-03-12 14:43:51 +0000

    fix spacing on heading alert

--- source/_partials/alert.blade.php
+++ source/_partials/alert.blade.php
@@ -1,4 +1,4 @@
-<!--<div class="alert alert-primary alert-warning dismissed">
+<!-- <div class="alert alert-primary alert-warning dismissed">
   <div class="container-fluid">
     <a title="Toggle" href="#" class="dismiss-alert btn btn-primary btn-sm"><span class="dismiss-alert-label">Show Details</span> <span class="dismiss-alert-toggle" aria-label="Toggle alert"></span></a>
     
@@ -9,4 +9,24 @@
       <p><a class="btn btn-primary" href="https://www.shoreline.edu/alerts.aspx">Learn more</a></p>
     </div>
   </div>
-</div>-->
+</div> -->
+
+<!-- <div class="alert alert-primary dismissed">
+  <div class="container-fluid">
+     <a title="Toggle" href="#" class="dismiss-alert btn btn-primary btn-sm">
+        <span class="dismiss-alert-label">Show Details</span>
+        <span class="dismiss-alert-toggle" aria-label="Toggle alert">
+     </span></a>
+     <div class="dismissed-expand-message">Coronavirus (COVID-19) Response</div>
+     <div class="alert-content">
+        <h2>Coronavirus (COVID-19) Response</h2>
+        <br>The College is open with <strong>most classes online</strong>
+        <strong>through March 25</strong>. Most campus services are operating normally. <br>
+        <br>
+        <a class="btn btn-primary btn-sm" href="/covid19.aspx">Stay informed</a>
+        <br>
+        <br>
+        <strong>Spring classes begin April 6.</strong>
+        <a href="/apply-and-aid/apply.aspx">Apply</a>, <a href="https://app.shoreline.edu/class-schedule/">browse classes</a>, <a href="/apply-and-aid/registration/">register</a>, and <a href="/apply-and-aid/cashiering/payment-methods.aspx">pay online</a>.</div>
+  </div>
+</div> -->
