34f187db538d3aa919aec80d7a9fe8a93a735f6b
virtual <jeanine.mt@gmail.com>
2020-06-15 20:24:48 +0000

    simplify mobile typography

--- source/_partials/headers.blade.php
+++ source/_partials/headers.blade.php
@@ -1,17 +1,19 @@
+<?php $class = !(empty($class)) ? $class : ''; ?>
+<div class="{{$class}}"></div>
 
-<h1>Lorem ipsum dolor sit amet </h1>
-<h2>Aenean commodo <a href="#">link</a> ligula eget </h2>
+<h1 class="{{$class}}">Lorem ipsum dolor sit amet </h1>
+<h2 class="{{$class}}">Aenean commodo <a href="#">link</a> ligula eget </h2>
 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing  
 montes, nascetur ridiculus mus. Donec quam felis, 
 ultricies nec, pellentesque eu, pretium quis, sem.</p> 
-<h3>Associate of Typography</h3>
+<h3 class="{{$class}}">Associate of Typography</h3>
 <p>Lorem ipsum dolor sit amet.</p>
-<h4>Associate of Typography</h4>
+<h4 class="{{$class}}">Associate of Typography</h4>
 <p>Lorem ipsum dolor sit amet.</p>
-<h5>Associate of Typography</h5>
+<h5 class="{{$class}}">Associate of Typography</h5>
 <p>Lorem ipsum dolor sit amet.</p>
-<h6>Associate of Typography</h6> 
+<h6 class="{{$class}}">Associate of Typography</h6> 
 <p>Lorem ipsum dolor sit amet.</p>
-<p class="small">Default / Hover</p>
+<p class="small {{$class}}">Default / Hover</p>
 <p>Stand out as a competitive applicant to four-year schools by
 <a href="#">graduating with Honors</a> on your transcript and diploma. Specialized seminars enhance the transfer process.</p>
