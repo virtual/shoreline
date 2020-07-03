5532ebebff170ba6a4118eb3aea69be47f034e6c
virtual <jeanine.mt@gmail.com>
2020-07-03 15:27:51 +0000

    offset footer map styling

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -5,7 +5,9 @@
       <div class="row">
         <div class="col-12 col-md-6 col-lg-6">
           <div class="footer-map">
-            map
+            <div class="bg-offset-bottom-right bg-offset-yellow-wave">
+              <div class="location-square-feature">Map</div>              
+            </div>
           </div>
         </div>
         <div class="col-12 col-md-6 col-lg-6">
