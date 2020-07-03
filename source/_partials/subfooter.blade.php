b736c84b8a36cf9cc11b474cd5f3f1fd693b3805
virtual <jeanine.mt@gmail.com>
2020-07-03 16:10:36 +0000

    style subfooter

--- source/_partials/subfooter.blade.php
+++ source/_partials/subfooter.blade.php
@@ -1,48 +1,32 @@
-<section class="subfooter bg-light py-5">
+<section class="subfooter bg-light">
   <div class="large-footer container-fluid">
     <div class="inner">
-        <div class="row">
-          <div class="col-12 col-md-6"> 
-          <h2>Ready to take the next step?</h2>
+      <div class="row">
+        <div class="col-12 col-md-6">
+          <h2>Ready to take the next step?</h2> 
 
-          
-            <ul class="list-unstyled list-inline footer-buttons">
-              <li><a class="btn btn-important" href="/apply-and-aid/apply.aspx">Get Started</a></li>
-              <li><a class="btn btn-link" href="/ask-us.aspx?source_page=https://www.shoreline.edu/">Contact</a></li> 
-              <li><a class="btn btn-link" href="/apply-and-aid/campus-tour.aspx">Visit Us</a></li>
-            </ul>
-            
-          </div>
-     
+          <ul class="pt-3 list-unstyled list-inline footer-buttons">
+            <li><a class="btn btn-important" href="/apply-and-aid/apply.aspx">Get Started</a></li>
+            <li><a class="btn btn-link" href="/ask-us.aspx?source_page=https://www.shoreline.edu/">Contact</a></li>
+            <li><a class="btn btn-link" href="/apply-and-aid/campus-tour.aspx">Visit Us</a></li>
+          </ul>
+        </div>
+
         <div class="col-12 col-md-6">
           <div class="links">
-            <ul class="list-unstyled">
-              <li>
-                <a href="/about-shoreline/hours-of-operation.aspx">
-                  Hours, Maps &amp; Directions
-                </a>
-              </li>
-              <li>
-                <a href="/sitemap/">A-Z Index</a>
-              </li>
-              <li>
-                <a href="https://www.shoreline.edu/campus-directory/">Employee Directory</a>
-              </li>
-              <li>
-                <a href="/hr/default.aspx">Jobs</a>
-              </li>
-              <li>
-                <a href="/about-shoreline/community-standard-statement.aspx">Community Standard</a>
-              </li>
-              <li>
-                <a href="https://shoreline.formstack.com/forms/website_feedback">Website Feedback</a>
-              </li>
+            <ul class="list-unstyled list-cols-2">
+              <li><a href="/about-shoreline/hours-of-operation.aspx">Hours, Maps &amp; Directions</a></li>
+              <li><a href="/hr/default.aspx">Jobs</a></li>
+              <li><a href="/sitemap/">Sitemap</a></li>
+              <li><a href="https://www.shoreline.edu/campus-directory/">Employee Directory</a></li>
+              <li><a href="#">Alumni</a></li>
+              <li><a href="#">Community Members</a></li>
+              <li><a href="#">Donors</a></li> 
+              <li><a href="https://shoreline.formstack.com/forms/website_feedback">Website Feedback</a></li>
             </ul>
-            
           </div>
-        </div> 
-     
+        </div>
       </div>
     </div>
-  </div> 
+  </div>
 </section>
