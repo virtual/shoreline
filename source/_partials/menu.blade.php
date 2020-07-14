b869127ee29185df3fc1f1b1e92e51bd1b1b4d1c
virtual <jeanine.mt@gmail.com>
2020-07-14 20:48:16 +0000

    button colors, put mobile search back

--- source/_partials/menu.blade.php
+++ source/_partials/menu.blade.php
@@ -12,27 +12,27 @@
         <img src="{{ $page->baseUrl }}/resources/img/logos/logo-full-mobile.png" srcset="{{ $page->baseUrl }}/resources/img/logos/logo-full-mobile2x.png 2x, {{ $page->baseUrl }}/resources/img/logos/logo-full-mobile3x.png 3x, {{ $page->baseUrl }}/resources/img/logos/logo-full-mobile4x.png 4x" alt="Shoreline Community College" />
       </a>
       <div class="flex-button flex-button-right">
-      <!-- <button id="mobile-search-button" class="navbar-toggler primary" type="button" data-toggle="collapse" data-target="#searchCollapse" aria-controls="searchCollapse"
+      <button id="mobile-search-button" class="navbar-toggler primary" type="button" data-toggle="collapse" data-target="#searchCollapse" aria-controls="searchCollapse"
         aria-expanded="false" aria-label="Toggle search">
         <span aria-hidden="true" class="fas fa-search"></span>
         <span class="sr-only">Search</span>
-      </button> -->
-      <p><a href="#" class="btn btn-sm btn-important">Get Started</a></p>
+      </button>
+      <!-- <p><a href="#" class="btn btn-sm btn-important">Get Started</a></p> -->
       </div>
     </div>
-    <!-- <div class="collapse navbar-collapse" id="searchCollapse">
+    <div class="collapse navbar-collapse" id="searchCollapse">
       <div class="container">
         <div class="searchform-mobile">
           @include ('_partials.searchform2')
         </div>
       </div>
-    </div> -->
+    </div>
     <div class="collapse navbar-collapse" id="navbarCollapse">
-      <div class="container-fluid d-block d-lg-none">
+      <!-- <div class="container-fluid d-block d-lg-none">
         <div class="searchform-mobile">
           @include ('_partials.searchform2')
         </div>
-      </div>
+      </div> -->
 
       <ul class="menu">
 
