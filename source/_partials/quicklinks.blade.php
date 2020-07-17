7d9f1e58ac4d59187a01efa940d0e44628d88727
virtual <jeanine.mt@gmail.com>
2020-07-17 15:15:27 +0000

    fix megamenu and mobile styling

--- source/_partials/quicklinks.blade.php
+++ source/_partials/quicklinks.blade.php
@@ -4,3 +4,10 @@
   <li><a href="/about-shoreline/contact-us.aspx">Contact</a></li> 
   <li><a href="/apply-and-aid/campus-tour.aspx">Visit</a></li>
 </ul>
+<ul class="quicklinks list-inline list-2">
+  <li class="has-button"><a class="btn btn-important btn-sm" href="/apply-and-aid/apply.aspx">Get Started</a></li>
+  <li class="has-button d-none d-lg-block">
+    <button class="btn btn-secondary btn-sm" id="searchtoggler" data-toggle="collapse" data-target="#searchmenusub" aria-controls="searchmenusub" aria-expanded="false"><span class="fa fa-search primary" aria-hidden="true"></span>
+      Search</button>
+  </li>
+</ul>
